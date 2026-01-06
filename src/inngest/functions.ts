a557b040aacb7e516a2f54c6168c054f50f1d3f9
yash00097 <yashwanthvuppala77@gmail.com>
2026-01-06 09:23:54 +0530

    implementedBackground-jobs using inngest

---
+++ src/inngest/functions.ts
@@ -0,0 +1,19 @@
+import prisma from "@/lib/db";
+import { inngest } from "./client";
+
+export const helloWorld = inngest.createFunction(
+  { id: "hello-world" },
+  { event: "test/hello.world" },
+  async ({ event, step }) => {
+    await step.sleep("wait-a-moment", "5s");
+    await step.sleep("wait-a-moment", "5s");
+    await step.sleep("wait-a-moment", "5s");
+    await step.run("create-workflow",() => {
+        return prisma.workflow.create({
+            data:{
+                name:"workflow-from-inngest",
+            }
+        })
+    })
+  },
+);
--- src/trpc/routers/_app.ts
+++ src/trpc/routers/_app.ts
@@ -1,13 +1,20 @@
 import prisma from '@/lib/db';
-import { baseProcedure, createTRPCRouter, protectedProcedure } from '../init';
+import { createTRPCRouter, protectedProcedure } from '../init';
+import { inngest } from '@/inngest/client';
+
 export const appRouter = createTRPCRouter({
-  getUsers: protectedProcedure.query(({ctx}) => {
-      return prisma.user.findMany({
-        where:{
-          id: ctx.auth.user.id
-        }
-      });
-    }),
+  getWorkflows: protectedProcedure.query(({ ctx }) => {
+    return prisma.workflow.findMany();
+  }),
+  createWorkflow: protectedProcedure.mutation(async()=> {
+    await inngest.send({
+      name: "test/hello.world",
+      data: {
+        email: "yashwanth.v23@iiits.in",
+      },
+    })
+    return { success: true , message: "Workflow queued!"};
+  }),
 });
 // export type definition of API
 export type AppRouter = typeof appRouter;
