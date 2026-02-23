37e66bdf1039569756108b8b1849562322e5d116
yash00097 <yashwanthvuppala77@gmail.com>
2026-02-23 11:54:49 +0530

    added manual trigger and http request nodes .Created node selector component.

--- src/config/node-components.ts
+++ src/config/node-components.ts
@@ -1,9 +1,13 @@
 import { InitialNode } from "@/components/initial-node";
+import { HttpRequestNode } from "@/features/executions/components/http-request/node";
+import { ManualTriggerNode } from "@/features/triggers/components/manual-trigger/node";
 import { NodeType } from "@/generated/prisma";
 import type { NodeTypes } from "@xyflow/react";
 
 export const nodeComponents = {
     [NodeType.INITIAL]: InitialNode,
+    [NodeType.MANUAL_TRIGGER]: ManualTriggerNode, 
+    [NodeType.HTTP_REQUEST]: HttpRequestNode, 
 } as const satisfies NodeTypes;
 
 export type RegisteredNodeTypes = keyof typeof nodeComponents;
