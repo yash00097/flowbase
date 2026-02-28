4c9c1eecd52695d7cac5544ddc12f028af30a9ec
yash00097 <yashwanthvuppala77@gmail.com>
2026-02-28 09:01:50 +0530

    created inngest channels and captured events using useNodeStatus.

--- src/features/executions/components/http-request/node.tsx
+++ src/features/executions/components/http-request/node.tsx
@@ -5,6 +5,9 @@ import { GlobeIcon } from "lucide-react";
 import { memo, useState } from "react";
 import { BaseExecutionNode } from "../base-execution-node";
 import { HttpRequestFormValues, HttpRequestDialog } from "./dialog";
+import { useNodeStatus } from "../../hooks/use-node-status";
+import { HTTP_REQUEST_CHANNEL_NAME } from "@/inngest/channels/http-request";
+import { fetchHttpRequestRealtimeToken } from "./actions";
 
 type HttpRequestNodeData = {
   variableName?: string;
@@ -20,7 +23,12 @@ export const HttpRequestNode = memo((props: NodeProps<HttpRequestNodeType>) => {
   const [dialogOpen, setDialogOpen] = useState(false);
   const { setNodes } = useReactFlow();
 
-  const NodeStatus = "initial"; 
+  const NodeStatus = useNodeStatus({
+    nodeId: props.id,
+    channel: HTTP_REQUEST_CHANNEL_NAME,
+    topic: "status",
+    refreshToken: fetchHttpRequestRealtimeToken,
+  });
   const handleOpenSettings = () => {
     setDialogOpen(true);
   }
--- src/features/triggers/components/manual-trigger/executor.ts
+++ src/features/triggers/components/manual-trigger/executor.ts
@@ -1,4 +1,6 @@
 import type { NodeExecutor } from "@/features/executions/types";
+import { manualTriggerChannel } from "@/inngest/channels/manual-trigger";
+
 
 type ManualTriggerData = Record<string, unknown>;
 
@@ -6,12 +8,23 @@ export const manualTriggerExecutor: NodeExecutor<ManualTriggerData> = async ({
   nodeId,
   context,
   step,
+  publish,
 }) => {
-  // TODO: Publish "loading" state for manual trigger
+  await publish(
+    manualTriggerChannel().status({
+      nodeId,
+      status: "loading",
+    }),
+  );
 
   const result = await step.run("manual-trigger", async () => context);
 
-  // TODO: Publish "success" state for manual trigger
+  await publish(
+    manualTriggerChannel().status({
+      nodeId,
+      status: "success",
+    }),
+  );
 
   return result;
 };
